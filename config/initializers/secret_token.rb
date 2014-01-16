054c95c083ce225e5ea09f4e504c037132d3dea5
Andres Yepes <andresyepes@Andress-MacBook-Air.local>
2014-01-16 15:55:28 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Musicoterapia::Application.config.secret_key_base = 'f530696ca1137b8979f61d5bbe18ea947963f58257682be870eb421333df6082fcedb2f91db663c6d31075c82c83788fd0807c58d08291778e68eb364c098bc1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Musicoterapia::Application.config.session_store :cookie_store, key: '_musicoterapia_session'
